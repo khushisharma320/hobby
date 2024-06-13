ddb70a90a2f82ae39a0dc815796dfa49c1e93f65
Khushi sharma <ks496627@gmail.com>
2024-06-13 10:53:31 +0530

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   
-        //... other configurations ...
-        // target: 'static', // Ensure this is set to 'static'
+eslint:{
+    ignoreDuringBuilds: true,
+},
       
 };
 
